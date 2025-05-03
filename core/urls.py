79cce54b113a7e0dadc583866f92036e819a1e35
dieg0andrees <diega.munoz@duocuc.cl>
2025-05-03 02:27:49 -0400

    base_added

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('',index,name="index"),
+    path('base/',base,name="base"),
+    path('about/',about,name="about"),
+    path('cart/',cart,name="cart"),
+    path('checkout/',checkout,name="checkout"),
+    path('contact/',contact,name="contact"),
+    path('shop_single/',shop_single,name="shop_single"),
+    path('shop/',shop,name="shop"),
+    path('thankyou/',thankyou,name="thankyou"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def cart(request):
+    return render(request, 'core/cart.html')
+
+def checkout(request):
+    return render(request, 'core/checkout.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def shop_single(request):
+    return render(request, 'core/shop-single.html')
+
+def shop(request):
+    return render(request, 'core/shop.html')
+
+def thankyou(request):
+    return render(request, 'core/thankyou.html')
+
+def base(request):
+    return render(request, 'core/base.html')
